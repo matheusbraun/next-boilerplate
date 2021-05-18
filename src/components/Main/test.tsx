fa0c4e59338e46390b4663b4ab0cf835efb9746c
Matheus Braun <matheeusbl@gmail.com>
2021-05-18 02:10:36 -0300

    adding configs

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,13 @@
+import { screen, render } from '@testing-library/react';
+
+import Main from '.';
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    render(<Main />);
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument();
+  });
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Braun!</a>
         </h1>
 
         <p className={styles.description}>
@@ -65,5 +65,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
